12f96d441b7d2c8709316f79c0fbda2855a09cdd
nathan-sharpe <sharpenn@mail.uc.edu>
2025-02-23 23:48:53 -0400

    Add project files.

---
+++ AdmirableAardvarks_Assignment06/main_package/main.py
@@ -0,0 +1,17 @@
+# File Name : main.py
+# Student Name: Nathan Sharpe
+# email: sharpenn@mail.uc.edu
+# Assignment Number: Assignment 06
+# Due Date: 2/27/2025
+# Course #/Section: IS 4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Collaborate with a group to add classes to a project and instantiate them and demonstrate
+# their functionality within main.
+
+# Brief Description of what this module does. Introduces us to OOP in python including classes, properties and methods.
+# Citations: In class work from 2/20/25 "VehicleClass"
+
+# Anything else that's relevant: I (Nathan Sharpe) handled main while Caitlin, Leah and Asfia created classes.
+
+if __name__ == "__main__":
+    pass
