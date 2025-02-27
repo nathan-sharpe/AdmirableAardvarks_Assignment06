825b0a73050f20ee00980dea42e4e2debe268dfe
nathan-sharpe <sharpenn@mail.uc.edu>
2025-02-26 21:43:24 -0400

    Added the main.py code to properly demonstrate the functionality of the banana class.

--- AdmirableAardvarks_Assignment06/main_package/main.py
+++ AdmirableAardvarks_Assignment06/main_package/main.py
@@ -13,6 +13,7 @@
 
 # Anything else that's relevant: I (Nathan Sharpe) handled main while Caitlin, Leah and Asfia created classes.
 from class_package.apple import Apple
+from class_package.banana import Banana
 
 if __name__ == "__main__":
     # Class 1: Apple created by Caitlin
@@ -22,20 +23,47 @@ if __name__ == "__main__":
     # Call the __str__ dunder method
     print(str(sweet_apple))
 
-    # Demonstrate the properties of the object with the getter
-    print(f"Apple's variety is  {sweet_apple.variety}")
-    print(f"Apple's weight is {sweet_apple.weight}")
-    print(f"Apple's ripeness is {sweet_apple.is_ripe}")
+    # Invoke the getter properties of the class
+    print(f"Sweet apple's variety is  {sweet_apple.variety}")
+    print(f"Sweet apple's weight is {sweet_apple.weight}")
+    print(f"Sweet apple's ripeness is {sweet_apple.is_ripe}")
 
     #Call the class method of ripen
     print(sweet_apple.ripen())
 
-    # Show methods effectiveness
-    print(f"Apple's ripeness now is {sweet_apple.is_ripe}")
+    # Show  the effectiveness of the method
+    print(f"Sweet apple's ripeness now is {sweet_apple.is_ripe}")
 
     # Invoke the setter to change the properties
-    sweet_apple.weight = 10
+    sweet_apple.weight = 15
     sweet_apple.variety = "Delicious"
+    sweet_apple.is_ripe = False
 
-    # Show effectiveness of the setter
+    # Show the effectiveness of the setter
     print(str(sweet_apple))
+
+    # Class 2: Banana created by Asfia 
+    # Create an instance of the class
+    yellow_banana = Banana(variety="Yellow", size = 20)
+
+    # Call the __str__ dunder method
+    print(str(yellow_banana))
+
+    # Invoke the getter properties of the class
+    print(f"Yellow Banana's variety is {yellow_banana.variety}")
+    print(f"Yellow Banana's size is {yellow_banana.size}")
+    print(f"Yellow Banana's ripeness level is {yellow_banana.ripeness_level}")
+
+    # Call the class method of ripen
+    print(yellow_banana.ripen())
+
+    # Show the effectiveness of the method
+    print(f"Yellow Banana's ripeness now is {yellow_banana.ripeness_level}")
+
+    # Invoke the setter to change the properties
+    yellow_banana.variety = "Green"
+    yellow_banana.size = 10
+    yellow_banana.ripeness_level = 0
+
+    # Show the effectiveness of the setter
+    print(str(yellow_banana))
